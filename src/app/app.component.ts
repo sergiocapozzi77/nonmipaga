9f1c6b916f8157514433689b027fab9e53e7a0cc
Sergio Capozzi <sergiocapozzi@outlook.com>
2021-02-25 19:33:03 +0000

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { EMPTY } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Non mi paga!';
+  constructor(private http: HttpClient) {}
+
+  title = "Non mi paga!";
 
   showAddPerson = false;
 
   toggleShowAddPerson() {
     this.showAddPerson = !this.showAddPerson;
+
+    console.log("No token");
+    this.http.get("http://localhost:3000/api/token/sign").subscribe(
+      (res) => {
+        console.log(res);
+        if (res["token"]) {
+          localStorage.setItem("token", res["token"]);
+          /*
+          this.http
+            .get("http://localhost:3000/api/furbetti/all")
+            .pipe(
+              catchError((er, c) => {
+                console.log("get furbetti error", er);
+                return EMPTY;
+              })
+            )
+            .subscribe((data) => {
+              console.log("get furbetti", data);
+            });*/
+        }
+      },
+      (err) => {
+        console.log("token error", err);
+      }
+    );
   }
 }
